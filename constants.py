f1a2b5355ea47472e022257417fb93ad1aa078c0
Lyxn-git <pepesergu@gmail.com>
2026-06-11 20:11:20 +0200

    Added constants.py

---
+++ constants.py
@@ -0,0 +1,26 @@
+
+# Constants for searching metal ions in
+S_METALS = ['Li', 'Na', 'K', 'Rb', 'Cs', 'Fr', 'Be', 'Mg', 'Ca', 'Sr', 'Ba', 'Ra']
+P_METALS = ['Al', 'Ga', 'In', 'Tl', 'Ge', 'Sn', 'Pb', 'Sb', 'Bi', 'Po']
+D3 = ["Sc", "Ti", "V", "Cr", "Mn", "Fe", "Co", "Ni", "Cu", "Zn"]
+D4 = ["Y", "Zr", "Nb", "Mo", "Tc", "Ru", "Rh", "Pd", "Ag", "Cd"]
+D5 = ["Hf", "Ta", "W", "Re", "Os", "Ir", "Pt", "Au", "Hg"]
+LN = ["La", "Ce", "Nd", "Pr", "Sm", "Eu", "Gd", "Tb", "Dy", "Ho", "Er", "Tm", "Yb", "Lu"]
+AN = ["Ac", "Th", "Pa", "U", "Np", "Pu", "Am", "Cm", "Bk", "Cf", "Es", "Fm", "Md", "No", "Lr"]
+
+REF_SHAPE_DICT = {2: "1 2 3",
+                  3: "1 2 3 4",
+                  4: "1 2 3 4",
+                  5: "1 2 3 4 5",
+                  6: "1 2 3 4 5",
+                  7: "1 2 3 4 5 6 7",
+                  8: "1 2 3 4 5 6 7 8 9 10 11 12 13",
+                  9: "1 2 3 4 5 6 7 8 9 10 11 12 13",
+                  10: "1 2 3 4 5 6 7 8 9 10 11 12 13",
+                  11: "1 2 3 4 5 6 7",
+                  12: "1 2 3 4 5 6 7 8 9 10 11 12 13",
+                  20: "1",
+                  24: "1 2",
+                  48: "1",
+                  60: "1"
+                } # This dict was created using the SHAPE 2.1 documentation
