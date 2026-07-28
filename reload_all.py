4609de9bef887d79ee6c673e22fa16f33dd85c81
Lyxn-git <pepesergu@gmail.com>
2026-07-28 17:31:53 +0200

    update reload_all.py

--- reload_all.py
+++ reload_all.py
@@ -3,14 +3,18 @@ import sys
 import os
 import olx
 
-base = os.path.join(olx.BaseDir(), 'util', 'pyUtil', 'PluginLib', 'plugin-SymmetryMeasurements')
-if base not in sys.path:
-    sys.path.insert(0, base)
+def reload_all():
+    base = os.path.join(olx.BaseDir(), 'util', 'pyUtil', 'PluginLib', 'plugin-SymmetryMeasurements')
+    if base not in sys.path:
+        sys.path.insert(0, base)
 
-for f in os.listdir(base):
-    if f.endswith('.py') and f != 'reload_all.py' and f != 'deprecated.py':
-        module_name = f[:-3]
-        if module_name in sys.modules:
-            importlib.reload(sys.modules[module_name])
-        else:
-            importlib.import_module(module_name)
+    for f in os.listdir(base):
+        if f.endswith('.py') and f != 'reload_all.py' and f != 'deprecated.py':
+            module_name = f[:-3]
+            if module_name in sys.modules:
+                importlib.reload(sys.modules[module_name])
+            else:
+                importlib.import_module(module_name)
+
+if __name__ == "__main__":
+    reload_all()
